c9536612eaca27c30558f142e06f38c0910a7f0a
abadalejandro <36165616+abadalejandro@users.noreply.github.com>
2022-10-24 14:09:01 +0200

    modified taskListComponent

--- src/components/container/taskListComponent.jsx
+++ src/components/container/taskListComponent.jsx
@@ -1,23 +1,27 @@
-import React from 'react';
-import { LEVELS } from '../../models/levels.enum';
-import { Task } from '../../models/task.class';
-import TaskComponent from '../pure/taskComponent';
-
+import React from "react";
+import { LEVELS } from "../../models/levels.enum";
+import { Task } from "../../models/task.class";
+import TaskComponent from "../pure/taskComponent";
 
 const TaskListComponent = () => {
-  const defaultTask = new Task('Example', 'Default description', false, LEVELS.NORMAL);
-
+  const defaultTask = new Task(
+    "Example",
+    "Default description",
+    false,
+    LEVELS.NORMAL
+  );
+  const changeState = (id) => {
+    console.log("TODO: cambiar estado de una tarea");
+  };
   return (
     <div>
       <div>
-        Your Tasks: 
+        <h1>Your Tasks:</h1>
       </div>
       <TaskComponent task={defaultTask}></TaskComponent>
-       {/* Aplicar un For/Map para renderizar una lista de tareas */}
+      {/* Aplicar un For/Map para renderizar una lista de tareas */}
     </div>
   );
-}
+};
 
 export default TaskListComponent;
-
-
